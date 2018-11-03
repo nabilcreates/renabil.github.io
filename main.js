8d3af35f996e1a05bae4b98d2783416da70d4d48
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-03 21:18:35 +0800

    Added base files

---
+++ main.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+
+    el: ".app",
+    data(){
+        return{
+            title: "hey",
+            apiData: [],
+            search: "",
+        }
+    },
+
+    computed: {
+        filteredSearch: function(){
+            return this.apiData.filter((data)=>{
+                return data.name.match(this.search);
+            })
+        }
+    },
+    
+    methods: {
+        getAPI(){
+            fetch("https://api.github.com/users/renabil/repos")
+            .then(res => res.json())
+            .then(json => {
+                this.apiData = json
+            })
+        }
+    },
+
+    mounted(){
+        this.getAPI()
+    }
+    
+})
