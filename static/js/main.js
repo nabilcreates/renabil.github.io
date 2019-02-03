51a6c596b771c8889a808013d9b08a44a66266b8
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-02-03 09:32:27 +0800

    Changed website format and added search

--- static/js/main.js
+++ static/js/main.js
@@ -4,16 +4,25 @@ let app = new Vue({
     data() {
         return {
             api_data: [],
+            search: '',
         }
     },
 
     methods: {
         fetch_api() {
-            fetch('https://api.github.com/users/renabil/repos?per_page=100')
+            fetch('https://api.github.com/users/renabil/repos?sort=recent&per_page=1000')
                 .then(response => response.json())
                 .then(json => {
                     this.api_data = json
                 })
+        }
+    },
+
+    computed: {
+        results: function(){
+            return this.api_data.filter((item) => {
+                return item.name.match(this.search)
+            })
         }
     },
 
