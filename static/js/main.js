5ccc444528bdf5cc6227eb897f6e68a337bdfecb
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-03-17 20:39:35 +0800

    *** ADDED REACT ***

--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,8 @@
-let app = new Vue({
-    el: '.app',
+// Render Logic
 
-    data() {
-        return {
-            api_data: [],
-            search: '',
-        }
-    },
-
-    methods: {
-        fetch_api() {
-            fetch('https://api.github.com/users/renabil/repos?sort=recent&per_page=1000')
-                .then(response => response.json())
-                .then(json => {
-                    this.api_data = json
-                })
-        }
-    },
-
-    computed: {
-        results: function(){
-            return this.api_data.filter((item) => {
-                return item.name.match(this.search)
-            })
-        }
-    },
-
-    created(){
-        this.fetch_api()
-    }
-
-})
+ReactDOM.render(
+    <div>
+        <App />
+    </div>,
+    document.getElementById('root')
+)
