6e95492ddc735800c3b6d734523a86ba4d46a583
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-03-17 19:33:03 +0800

    Added fontawesome 4.7.0

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component{
     componentDidMount() {
         this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
     }
-    
+
     componentWillUnmount() {
       clearInterval(this.interval);
     }
@@ -17,6 +17,17 @@ class Header extends React.Component{
                 <div className='container'>
                     <p>{date}</p>
                     <h1>NABIL</h1>
+
+                    <div className='linkButtons'>
+                    
+                    <a href='https://github.com/renabil'><i className="fa fa-github linkicon" ariaHidden='true'></i></a>
+
+                    <a href='https://instagram.com/nab1lll'><i className="fa fa-instagram linkicon" ariaHidden='true'></i></a>
+
+                    <a href='https://renabil.github.io'><i className="fa fa-pencil-square linkicon" ariaHidden='true'></i></a>
+                    
+                    </div>
+                    
                 </div>
         
             </div>
