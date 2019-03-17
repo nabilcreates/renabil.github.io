aad531325a29b30f3b2a1c4d22a65dfa0a1979b0
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-03-17 20:09:29 +0800

    Added created date

--- components/Repo.js
+++ components/Repo.js
@@ -10,14 +10,19 @@ class Repo extends React.Component{
         let repo =  this.props.repo
 
         let renderGithubPages;
+        let repodate;
 
         if(repo.has_pages){
             renderGithubPages = <a className='pageLink' href={`https://renabil.github.io/` + repo.name}>Page</a>
         }
+
+        repodate = new Date(repo.created_at).toDateString().toLocaleUpperCase().slice(4)
+
         
         return(
             <div className='repo'>
                 <div className='container'>
+                    <p className='repodate'>CREATED ON {repodate}</p>
                     <h1>{repo.name}</h1>
                     <p>{repo.description}</p>
                     <a className='repoLink' href={repo.html_url}>Repository</a>
