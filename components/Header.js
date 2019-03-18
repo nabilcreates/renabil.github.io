3e0cf5c7542510046b3cf1e2f173f0f635156393
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-03-18 18:26:41 +0800

    Changed Header and implemented CSS Grid for the header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,13 @@
 class Header extends React.Component{
 
     // ALLOWS THE CODE TO UPDATE OR REMOUNT EVERY SECOND
-    componentDidMount() {
-        this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
-    }
+    // componentDidMount() {
+    //     this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
+    // }
 
-    componentWillUnmount() {
-      clearInterval(this.interval);
-    }
+    // componentWillUnmount() {
+    //   clearInterval(this.interval);
+    // }
 
     render(){
         let date = new Date().toLocaleTimeString()
@@ -15,18 +15,25 @@ class Header extends React.Component{
         return(
             <div className='header'>
                 <div className='container'>
-                    <p>{date}</p>
-                    <h1>NABIL</h1>
-
-                    <div className='linkButtons'>
                     
-                    <a href='https://github.com/renabil'><i className="fa fa-github linkicon"></i></a>
+                    <div className='top-header'>
 
-                    <a href='https://instagram.com/nab1lll'><i className="fa fa-instagram linkicon"></i></a>
-
-                    <a href='https://renabil.github.io'><i className="fa fa-pencil-square linkicon"></i></a>
+                        <div className='jobs'>
+                            <p><i className="fa fa-map-marker"></i> SINGAPORE</p>
+                        </div>
                     
+                        <h1>NABIL</h1>
+                        <div className='linkButtons'>
+                        
+                        <a href='https://github.com/renabil'><i className="fa fa-github linkicon"></i></a>
+
+                        <a href='https://instagram.com/nab1lll'><i className="fa fa-instagram linkicon"></i></a>
+
+                        <a href='https://renabil.github.io'><i className="fa fa-pencil-square linkicon"></i></a>
+                        
+                        </div>
                     </div>
+
                     
                 </div>
         
