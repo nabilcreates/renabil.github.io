82397051e8c54d82ba6d065f716e953d1828cbff
Nabil Ridhwanshah <nabilrdhwan@gmail.com>
2019-08-04 16:25:05 +0800

    Ground Zero

--- README.md
+++
@@ -1,4 +0,0 @@
-# RENABIL
-
-## Updated on 17 March 2019
--   My website now runs on React rather than Vue which is a drastic change, While UI right now doesn't look beautiful, I will work on it!
---
+++ static/main.js
@@ -0,0 +1,27 @@
+async function return_recent_repos(username) {
+    let return_array = [];
+
+    await fetch('https://api.github.com/users/' + username + '/repos?sort=updated&per_page=100')
+        .then(response => response.json())
+        .then(json => {
+
+            for(let i = 0; i < 3; i ++){
+                return_array.push(json[i])
+            }
+
+        })
+
+        return return_array;
+}
+
+async function return_repos(username) {
+    let return_array = [];
+
+    await fetch('https://api.github.com/users/' + username + '/repos?sort=fullname&per_page=100')
+        .then(response => response.json())
+        .then(json => {
+            return_array = json;
+        })
+
+        return return_array;
+}
